511e8193b8346cae2ef8ed7028edaf494e8990c5
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-06-10 16:45:50 -0400

    Finished objects.js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,32 +5,40 @@
 // };
 // myFunction();
 
-let myFunction = () => console.log("Function was Invoked");
-myFunction();
+//  ANSWER **********************************************
+
+// let myFunction = () => console.log("Function was Invoked");
+// myFunction();
 
 // let anotherFunction = function(param) {
 //   return param;
 // };
 // anotherFunction("Example");
 
-let anotherFunction = param => param;
-console.log(anotherFunction("Example"));
+//  ANSWER *******************************************
+
+// let anotherFunction = param => param;
+// console.log(anotherFunction("Example"));
 
 // let add = function(param1, param2) {
 //   return param1 + param2;
 // };
 // add(1, 2);
 
-let add = (param1, param2) => param1 + param2;
-console.log(add(1, 2));
+// ANSWER **************************************
+
+// let add = (param1, param2) => param1 + param2;
+// console.log(add(1, 2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
-let subtract = (param1, param2) => param1 - param2;
-console.log(subtract(1, 2));
+// ANSWER ****************************************
+
+// let subtract = (param1, param2) => param1 - param2;
+// console.log(subtract(1, 2));
 
 // Stretch
 
@@ -40,6 +48,8 @@ console.log(subtract(1, 2));
 // });
 // console.log(triple);
 
-exampleArray = [1, 2, 3, 4];
-const triple = exampleArray.map(num => num * 3);
-console.log(triple);
+// ANSWER *******************************************
+
+// exampleArray = [1, 2, 3, 4];
+// const triple = exampleArray.map(num => num * 3);
+// console.log(triple);
